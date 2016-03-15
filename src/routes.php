36dbfd658f027777cc98785167c378b67592645d
Srdjan Sin Jovanovic <mp4065@gmail.com>
2016-03-15 21:52:41 +0100

    changed for laravel 5.3

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,47 @@ use Illuminate\Support\Facades\Route;
 Route::group(
     ['middleware' => ['web']],
     function () {
-        Route::auth();
+        Route::get(
+            '/register',
+            ['as'   => 'register',
+             'uses' => 'mp3063\MailActivation\controllers\AuthWithActivationController@getRegister',
+            ]
+        );
+        Route::post(
+            '/register',
+            ['as'   => 'register',
+             'uses' => 'mp3063\MailActivation\controllers\AuthWithActivationController@postRegister',
+            ]
+        );
+        Route::get(
+            '/login',
+            ['as'   => 'login',
+             'uses' => 'mp3063\MailActivation\controllers\AuthWithActivationController@getLogin',
+            ]
+        );
+        Route::post(
+            '/login',
+            ['as'   => 'login',
+             'uses' => 'mp3063\MailActivation\controllers\AuthWithActivationController@postLogin',
+            ]
+        );
+        Route::get(
+            '/logout',
+            ['as'   => 'logout',
+             'uses' => 'mp3063\MailActivation\controllers\AuthWithActivationController@getLogout',
+            ]
+        );
+        Route::get(
+            '/forgot',
+            ['as'   => 'forgot',
+             'uses' => 'mp3063\MailActivation\controllers\AuthWithActivationController@getLogin',
+            ]
+        );
+        Route::post(
+            '/forgot',
+            ['as'   => 'forgot',
+             'uses' => 'mp3063\MailActivation\controllers\AuthWithActivationController@postLogin',
+            ]
+        );
     }
 );
